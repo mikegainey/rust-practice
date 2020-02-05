372816013b5ccea2b85936437085477d5408ec2d
Mike Gainey <mikeg9b@gmail.com>
2020-02-05 16:45:42 -0600

    chapter 3

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,11 @@ fn main() {
         println!("x = {}\n", x);
     }
 
+
     {
-        // integer literals
+        // Integer types default to i32: this type is generally the fastest, even on 64-bit systems.
+        // The primary situation in which you’d use isize or usize is when indexing some sort of collection.
+
         let a = 123_456; // with a digit separator
         let b = 0xff; // hex
         let c = 0b1111_0000; // binary
@@ -19,7 +22,38 @@ fn main() {
         println!("{} {} {} {} {}\n", a, b, c, d, e);
     }
 
-    // Integer types default to i32: this type is generally the fastest, even on 64-bit systems.
-    // The primary situation in which you’d use isize or usize is when indexing some sort of collection.
 
+    {
+        // character literals
+
+        let c = 'z';
+        let z = 'ℤ';
+        let heart_eyed_cat = '😻';
+        println!("{} {} {}\n", c, z, heart_eyed_cat);
+    }
+
+    {
+        // tuples
+
+        let tup = (500, 6.4, 1);
+        println!("{} {} {}", tup.0, tup.1, tup.2);
+
+        let (x, y, z) = tup; // destructuring
+        println!("{} {} {}\n", x, y, z);
+    }
+
+    {
+        // Arrays are useful when you want your data allocated on the stack rather than the heap
+
+        let a: [i32; 5] = [1, 2, 3, 4, 5];
+        let first = a[0];
+        let second = a[1];
+        println!("a[0] = {}, a[1] = {}", first, second);
+
+        let b = [3; 5]; // same as [3, 3, 3, 3, 3]
+        println!("{:?}\n", b);
+    }
+
+
+    println!("Chapter 3.3: Functions\n");
 }
